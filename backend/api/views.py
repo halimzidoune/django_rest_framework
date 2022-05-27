73221cbb5bdae3b2748d53869440537a5be346b1
halimzidoune <halimzidoune@gmail.com>
2022-05-27 22:39:37 +0100

    Send and Receive Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,24 @@
 import imp
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({
-        "message": "Hello wolrd !"
-    })
+    data = {}
+    body = request.body
+    
+    print(body)
+
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+    # print(data.keys())  # -> show params names sended in json
+    print(request.headers)
+    data['content_type'] = request.content_type
+    data['headers'] = dict(request.headers)
+    data['params'] = request.GET
+    data['name'] = request.GET['name']
+
+    return JsonResponse(data)
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,12 +1,12 @@
 import requests
 
 #endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/api/"
 
-response = requests.get(endpoint)
+response = requests.get(endpoint, params={"name": "josef"}, json={"query": "Hello"})
 
 # print(response.text)
 print(response.json())
-print(response.json()["message"])
+#print(response.json()["message"])
 print(response.status_code)
 
