94740a04af4073aad76b3dda19dbbe106190c97f
halimzidoune <halimzidoune@gmail.com>
2022-05-27 22:06:18 +0100

    Send Request to local server

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,12 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
+#endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api"
 
 response = requests.get(endpoint)
 
 # print(response.text)
 print(response.json())
+print(response.json()["message"])
 print(response.status_code)
 
